655e35b2a4f7cfa7c520b97f6dae6f0861a03036
Benjamin Shelton <Bshel419@rocketmail.com>
2021-04-07 21:48:28 -0500

    added beginnings of Ina's website! (first real project)

---
+++ Ina's Website/js/stuff.js
@@ -0,0 +1,65 @@
+playDigging()
+{
+    var video = document.getElementById('digging');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
+
+playCollar()
+{
+    var video = document.getElementById('collarVid');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
+
+playFrisbee()
+{
+    var video = document.getElementById('frisbeeVid');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
+
+playNap()
+{
+    var video = document.getElementById('napVid');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
+
+playWater()
+{
+    var video = document.getElementById('waterVid');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
+
+playWobbler()
+{
+    var video = document.getElementById('wobbler');
+    video.play();
+    video.addEventListener('ended', 
+    function()
+    {
+        window.location = '../html/mainPage.html';
+    });
+}
